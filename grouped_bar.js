582f68cee035d9c7d6cf6b3741defa234b663ff2
mjasher <michael.james.asher@gmail.com>
2014-11-10 15:03:09 +1100

    for C

--- grouped_bar.js
+++ grouped_bar.js
@@ -17,7 +17,8 @@ function grouped_bar(){
 
   var color = d3.scale.ordinal()
       // .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-      .range(["#1f77b4",  "#2ca02c"]);
+      // .range(["#1f77b4",  "#2ca02c"]);
+      .range(["steelblue",  "darkred"]);
   // var color = d3.scale.category10();
 
   var xAxis = d3.svg.axis()
